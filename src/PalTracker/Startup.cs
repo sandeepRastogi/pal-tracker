5541ff965fed13e6f207d9c1d1a1cd57bcd9e3bd
Sandeep_Rastogi <Sandeep_Rastogi@dell.com>
2019-07-29 15:49:00 +0530

    

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -26,6 +26,13 @@ namespace PalTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton(sp => new WelcomeMessage( Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.") ));
+            services.AddSingleton(sp => new CloudFoundryInfo( 
+                Configuration.GetValue<string>("PORT", "NOT SET"),
+                Configuration.GetValue<string>("MEMORY_LIMIT", "1111"),
+                Configuration.GetValue<string>("CF_INSTANCE_INDEX", "1111"),
+                Configuration.GetValue<string>("CF_INSTANCE_ADDR", "1111")
+                 ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
